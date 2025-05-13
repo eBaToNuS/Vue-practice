7bcbe70745685447867ec6cd04a85736e4d182d3
eBaToNuS <Sulzhenko_10012004@mail.ru>
2025-05-13 12:09:27 +0300

    Firts commit

---
+++ layout/AuthLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container with-nav">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Auth from "@/views/Auth.vue";
+export default {
+  components: { Auth },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ layout/MainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <the-navbar></the-navbar>
+    <div class="container with-nav">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheNavbar from "../TheNavbar.vue";
+export default {
+  components: { TheNavbar },
+};
+</script>
+
+<style lang="scss" scoped></style>
